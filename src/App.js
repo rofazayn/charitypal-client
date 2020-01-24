be368dad1d8cd7cfb3435695f91715f9715e9427
Abderraouf Zine <rofazayn@gmail.com>
2020-01-24 14:44:52 +0000

    Initial commit from Rauf.

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className='App'>
+      <h1 style={{ textAlign: 'center' }}>CharityPal.</h1>
+    </div>
+  );
+}
+
+export default App;
