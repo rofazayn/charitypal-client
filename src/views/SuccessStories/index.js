bc87e976c7bca25b2a476640651f8701fb9f92fb
Abderraouf Zine <rofazayn@gmail.com>
2020-01-29 10:02:38 +0000

    Built the success storie page.

--- src/views/SuccessStories/index.js
+++ src/views/SuccessStories/index.js
@@ -1,12 +1,44 @@
 import React from 'react';
 import { Styled } from './style';
-import { Container, Typography } from '@material-ui/core';
+import { Container, Typography, Grid } from '@material-ui/core';
+import CPButton from '../../components/layout/CPButton';
+// import { Link } from 'react-router-dom';
+import { ReactComponent as SuccessSvg } from '../../assets/svg/undraw_celebration_0jvk.svg';
 
 const SuccessStories = () => {
   return (
     <Styled.SuccessStories className='page'>
       <Container>
-        <Typography variant='h3'>Success stories</Typography>
+        <Grid
+          container
+          spacing={10}
+          alignItems='center'
+          justify='space-between'
+        >
+          <Grid item md={6} className='hero'>
+            <Typography variant='h4' className='hero__title'>
+              Pals' success <span className='highlight'>stories!</span>
+            </Typography>
+            <Typography variant='body1' className='hero__paragraph'>
+              We’ve all got one. A thing we love doing, a thing we’ve always
+              wanted to do or just a thing we could do. Whatever it is, when we
+              do it, we can really make a difference. Do your thing and change
+              people’s lives, then tell your story.
+            </Typography>
+            <div className='hero__buttons'>
+              {/* <Link to='/donate'> */}
+              <CPButton variant='contained' color='primary'>
+                Tell your story now
+              </CPButton>
+              {/* </Link> */}
+            </div>
+          </Grid>
+          <Grid item md={6} className='image'>
+            <div className='custom-image'>
+              <SuccessSvg />
+            </div>
+          </Grid>
+        </Grid>
       </Container>
     </Styled.SuccessStories>
   );
--- src/views/SuccessStories/style.js
+++ src/views/SuccessStories/style.js
@@ -1,6 +1,25 @@
 import styled from 'styled-components';
 
-const SuccessStories = styled.div``;
+const SuccessStories = styled.div`
+  .hero {
+    max-width: 600px;
+    &__title {
+      margin-bottom: ${({ theme }) => `${theme.spacing(3)}px`};
+    }
+    &__paragraph {
+      margin-bottom: ${({ theme }) => `${theme.spacing(3)}px`};
+    }
+    &__buttons {
+      button:first-of-type {
+        margin-inline-end: ${({ theme }) => `${theme.spacing(2)}px`};
+      }
+      margin-bottom: ${({ theme }) => `${theme.spacing(3)}px`};
+    }
+    &__caption {
+      color: ${({ theme }) => theme.palette.grey[700]};
+    }
+  }
+`;
 
 export const Styled = {
   SuccessStories
