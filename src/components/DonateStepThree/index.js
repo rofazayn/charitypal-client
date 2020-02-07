60d0de97cecdc363d9d25f5922f38160f919b3c7
Abd Rauf Zayn <rofazayn@gmail.com>
2020-02-07 10:35:20 +0100

    Fixed some small ui issues.

--- src/components/DonateStepThree/index.js
+++ src/components/DonateStepThree/index.js
@@ -1,33 +1,27 @@
-import React from 'react';
-import { Grid, Typography } from '@material-ui/core';
-import { motion } from 'framer-motion';
-import { ReactComponent as StepThreeSvg } from '../../assets/svg/undraw_festivities_tvvj.svg';
+import React from "react";
+import { Grid, Typography } from "@material-ui/core";
+import { motion } from "framer-motion";
+import { ReactComponent as StepThreeSvg } from "../../assets/svg/undraw_festivities_tvvj.svg";
 
 const DonateStepThree = ({ nextStep, data, dispatch }) => {
   return (
-    <Grid container spacing={10} alignItems='center' justify='space-between'>
-      <Grid item md={6} className='image'>
-        <div className='custom-image'>
-          <motion.div
-            initial={{ opacity: 0, scale: 0.8 }}
-            exit={{ opacity: 0, scale: 0.8 }}
-            animate={{ opacity: 1, scale: 1 }}
-          >
-            <StepThreeSvg />
-          </motion.div>
+    <Grid container spacing={10} alignItems="center" justify="space-between">
+      <Grid item md={6} className="image">
+        <div className="custom-image">
+          <StepThreeSvg />
         </div>
       </Grid>
-      <Grid item md={6} className='hero'>
+      <Grid item md={6} className="hero">
         <motion.div
           initial={{ opacity: 0 }}
           exit={{ opacity: 0 }}
           animate={{ opacity: 1 }}
         >
-          <Typography variant='h4' className='hero__title'>
+          <Typography variant="h4" className="hero__title">
             Thank you for <br />
             your generosity!
           </Typography>
-          <Typography variant='body1' className='hero__paragraph'>
+          <Typography variant="body1" className="hero__paragraph">
             Your donation is being processed, your credit card will be charged
             shortly and a receipt will be sent to your email.
           </Typography>
